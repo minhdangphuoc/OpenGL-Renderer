ff6748ecf03cc58137680138af78370692bd0e56
minhdangphuoc <cunminh1405@gmail.com>
2022-07-23 17:40:17 +0300

    Fixing: git ignore

---
+++ src/GLRenderer.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#define GL_RENDERER_HPP 
+#ifdef GL_RENDERER_HPP
+class Render
+{
+    public:
+    private:
+};
+#endif // GL_RENDERER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,11 @@ namespace Window
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
-
+        
+        #ifdef __APPLE__
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        #endif
+        
         return true;
     }
 
