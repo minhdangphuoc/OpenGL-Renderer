e92c32fb7ff8495419b3e3cf9defc2d4b741a1dc
minhdangphuoc <cunminh1405@gmail.com>
2022-07-30 18:46:24 +0300

    Added: some demos

--- src/GLRenderer.cpp
+++ src/GLRenderer.cpp
@@ -140,29 +140,20 @@ void GLRenderer::draw()
 
     glBindTexture(GL_TEXTURE_2D, texture);
     ourShader->use();
-    
-    
-    glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-    // first container
-    // ---------------
-    transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-    transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-    // get their uniform location and set matrix (using glm::value_ptr)
-    unsigned int transformLoc = glGetUniformLocation(ourShader->ID, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+    glm::mat4 transform;
+    for (int i = 1; i <= 30; i++)
+    {
+        transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        // first container
+        // ---------------
+        transform = glm::rotate(transform, (float)glfwGetTime() * (i/30.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        // get their uniform location and set matrix (using glm::value_ptr)
+        unsigned int transformLoc = glGetUniformLocation(ourShader->ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
-    // with the uniform matrix set, draw the first container
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    // second transformation
-    // ---------------------
-    transform = glm::mat4(1.0f); // reset it to identity matrix
-    transform = glm::translate(transform, glm::vec3(-0.5f, 0.5f, 0.0f));
-    float scaleAmount = static_cast<float>(sin(glfwGetTime()));
-    transform = glm::scale(transform, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &transform[0][0]); // this time take the matrix value array's first element as its memory pointer value
-
-    // now with the uniform matrix being replaced with new transformations, draw it again.
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // with the uniform matrix set, draw the first container
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    }
 }
